7ea03f8e2004b242d4c841bfd5248e5660a724b6
Liliya <lilyeka@gmail.com>
2019-04-19 18:38:59 +0500

    files

---
+++ DesignableViewsAndInspectableProperties/MyView.swift
@@ -0,0 +1,37 @@
+//
+//  MyView.swift
+//  DesignableViewsAndInspectableProperties
+//
+//  Created by Лилия Левина on 19/04/2019.
+//  Copyright © 2019 Лилия Левина. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class MyView: UIView {
+    func configure() {
+        self.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+        let v2 = UIView()
+        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
+        let v3 = UIView()
+        v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        v2.translatesAutoresizingMaskIntoConstraints = false
+        v3.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(v2)
+        self.addSubview(v3)
+        NSLayoutConstraint.activate([
+            v2.leftAnchor.constraint(equalTo:self.leftAnchor),
+            v2.rightAnchor.constraint(equalTo:self.rightAnchor),
+            v2.topAnchor.constraint(equalTo:self.topAnchor),
+            v2.heightAnchor.constraint(equalToConstant:20),
+            v3.widthAnchor.constraint(equalToConstant:20),
+            v3.heightAnchor.constraint(equalTo:v3.widthAnchor),
+            v3.rightAnchor.constraint(equalTo:self.rightAnchor),
+            v3.bottomAnchor.constraint(equalTo:self.bottomAnchor),
+            ])
+    }
+    override func willMove(toSuperview newSuperview: UIView!) {
+        self.configure()
+    }
+}
+
--- DesignableViewsAndInspectableProperties/ViewController.swift
+++ DesignableViewsAndInspectableProperties/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let myView = MyView(frame:CGRect(x:100, y:111, width:132, height:194))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(myView)
     }
 
 
